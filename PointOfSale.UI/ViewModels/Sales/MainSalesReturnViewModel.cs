e547f2735662b3ce33cf31bcf397b70a77d0e1e3
dilwera <76738656+DilharaDenuwanDiwakara@users.noreply.github.com>
2026-09-14 19:49:29 +0530

    Change: Sales Summary Report

--- PointOfSale.UI/ViewModels/Sales/MainSalesReturnViewModel.cs
+++ PointOfSale.UI/ViewModels/Sales/MainSalesReturnViewModel.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using PointOfSale.Core.DTOs;
 using PointOfSale.Core.Interfaces.Repositories.Sales;
 using PointOfSale.Core.Interfaces.Services;
 using PointOfSale.Core.Models.Sales;
 using PointOfSale.Core.Services;
-using PointOfSale.Infrastructure.Repositories.Sales;
 using PointOfSale.UI.Commands;
 
 namespace PointOfSale.UI.ViewModels.Sales
--- PointOfSale.UI/ViewModels/Sales/SalesReturnModel.cs
+++ PointOfSale.UI/ViewModels/Sales/SalesReturnModel.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PointOfSale.Core.DTOs;
 using PointOfSale.Core.Models.Sales;
 
 namespace PointOfSale.UI.ViewModels.Sales
